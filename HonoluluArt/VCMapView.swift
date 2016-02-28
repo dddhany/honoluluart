19d9a94af97a3d9b2b1c3948f67d6e179fe03c05
Dhany Basuki <dddhany+github@gmail.com>
2016-02-28 23:12:44 +0800

    map annotation

---
+++ HonoluluArt/VCMapView.swift
@@ -0,0 +1,32 @@
+//
+//  VCMapView.swift
+//  HonoluluArt
+//
+//  Created by Dhany Basuki on 28/2/16.
+//  Copyright © 2016 Dhany. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? Artwork {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+            }
+            return view
+        }
+        return nil
+    }
+}
--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    let regionRadius: CLLocationDistance = 1000
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(initialLocation)
+        
+        // show artwork on map
+        mapView.delegate = self
+        let artwork = Artwork(title: "King David Kalakaua",
+            locationName: "Waikiki Gateway Park",
+            discipline: "Sculpture",
+            coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        
+        mapView.addAnnotation(artwork)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
-
 
 }
 
